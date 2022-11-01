c5a36407141cf88de207315801fcc6a0cce44cd7
Dmitrijs Fofanovs <dmitrijs.f.92@outlook.com>
2022-11-01 23:53:12 +0200

    Update 05-phone-key-pad.php

--- 03-homework-03.11/02-flow-of-control/05-phone-key-pad.php
+++ 03-homework-03.11/02-flow-of-control/05-phone-key-pad.php
@@ -8,67 +8,102 @@
             case 'A':
             case 'B':
             case 'C':
-                $output.='2';
+                for($i='A';$i<=$element;$i++) {
+                    $output .= '2';
+                }
                 break;
             case 'D':
             case 'E':
             case 'F':
-                $output.='3';
+                for($i='D';$i<=$element;$i++) {
+                    $output .= '3';
+                }
                 break;
             case 'G':
             case 'H':
             case 'I':
-                $output.='4';
+                for($i='G';$i<=$element;$i++) {
+                    $output .= '4';
+                }
                 break;
             case 'J':
             case 'K':
             case 'L':
-                $output.='5';
+                for($i='J';$i<=$element;$i++) {
+                    $output .= '5';
+                }
                 break;
             case 'M':
             case 'N':
             case 'O':
-                $output.='6';
+                for($i='M';$i<=$element;$i++) {
+                    $output .= '6';
+                }
                 break;
             case 'P':
             case 'Q':
             case 'R':
             case 'S':
-                $output.='7';
+                for($i='P';$i<=$element;$i++) {
+                    $output .= '7';
+                }
                 break;
             case 'T':
             case 'U':
             case 'V':
-                $output.='8';
+                for($i='T';$i<=$element;$i++) {
+                    $output .= '8';
+                }
                 break;
             case 'W':
             case 'X':
             case 'Y':
             case 'Z':
-                $output.='9';
+                for($i='W';$i<=$element;$i++) {
+                    $output .= '9';
+                }
                 break;
             default : break;
         }
+        $output.=' ';
     }
     echo 'switch case: '.$output . PHP_EOL;
 
     $output='';
     foreach($keypadLetters as $element){
         if($element === 'A' || $element === 'B' || $element === 'C'){
-            $output.='2';
+            for($i='A';$i<=$element;$i++) {
+                $output .= '2';
+            }
         } else if($element === 'D' || $element === 'E' || $element === 'F'){
-            $output.='3';
+            for($i='D';$i<=$element;$i++) {
+                $output .= '3';
+            }
         } else if($element === 'G' || $element === 'H' || $element === 'I'){
-            $output.='4';
+            for($i='G';$i<=$element;$i++) {
+                $output .= '4';
+            }
         } else if($element === 'J' || $element === 'K' || $element === 'L'){
-            $output.='5';
+            for($i='J';$i<=$element;$i++) {
+                $output .= '5';
+            }
         } else if($element === 'M' || $element === 'N' || $element === 'O'){
-            $output.='6';
+            for($i='M';$i<=$element;$i++) {
+                $output .= '6';
+            }
         } else if($element === 'P' || $element === 'Q' || $element === 'R' || $element === 'S'){
-            $output.='7';
+            for($i='P';$i<=$element;$i++) {
+                $output .= '7';
+            }
         } else if($element === 'T' || $element === 'U' || $element === 'V'){
-            $output.='8';
+            for($i='T';$i<=$element;$i++) {
+                $output .= '8';
+            }
         } else if($element === 'W' || $element === 'Y' || $element === 'Z' || $element === 'X' ){
-            $output.='9';
+            for($i='W';$i<=$element;$i++) {
+                $output .= '9';
+            }
         }
+        $output.=' ';
     }
+    echo 'nested if: '. $output . PHP_EOL;

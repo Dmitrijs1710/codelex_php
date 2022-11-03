b2748954eec7574180e42e91e31ad35cc0e041b4
Dmitrijs Fofanovs <dmitrijs.f.92@outlook.com>
2022-11-03 11:18:23 +0200

    Update 06-hangman.php

--- 02-homework-01.11/02-array/06-hangman.php
+++ 02-homework-01.11/02-array/06-hangman.php
@@ -1,54 +1,66 @@
 <?php
 
     $words = ['word', 'codelex', 'wordwrap', 'trait'];
-    $word = $words[(array_rand($words))];
-    $maxGuesses = 7;
-    $guesses = 0;
+    while(true){
+        $word = $words[(array_rand($words))];
+        $maxGuesses = 7;
+        $guesses = 0;
 
-    $wordLetters = str_split($word);
-    $guessedLetters = [];
-    for ($i = 0; $i < count($wordLetters); $i++) {
-        $guessedLetters[] = "_";
-    }
-    $misses = [];
-    $guess = [];
+        $wordLetters = str_split($word);
+        $guessedLetters = [];
+        for ($i = 0; $i < count($wordLetters); $i++) {
+            $guessedLetters[] = "_";
+        }
+        $misses = [];
+        $guess = [];
 
 
-    while ($guesses < $maxGuesses && in_array('_', $guessedLetters)) {
-        echo str_repeat('-=',10) . '-' . PHP_EOL;
-        echo PHP_EOL;
-        echo "Word: " . implode(' ', $guessedLetters) . PHP_EOL;
-        echo PHP_EOL;
-        echo "Misses: " . implode(' ',$misses) . PHP_EOL;
-        echo PHP_EOL;
-        echo "Guess: " . implode(' ',$guess) . PHP_EOL;
-        while(true){
+        while ($guesses < $maxGuesses && in_array('_', $guessedLetters)) {
+            echo str_repeat('-=',10) . '-' . PHP_EOL;
             echo PHP_EOL;
-            $input = strtolower(readline('Input a char : '));
-            if (preg_match('/^[A-Z]$/i',$input)){
-                if(!in_array($input,$guess)&&!in_array($input,$misses)){
-                    break;
-                } else echo "Char already has been used. Try another!\n";
-            } else echo "Incorrect input. Input 1 char!\n";
-        }
-
-        if (in_array($input, $wordLetters)) {
-            $guess[] = $input;
-            foreach ($wordLetters as $key => $element) {
-                if ($element === $input) {
-                    $guessedLetters[$key] = $input;
-                }
+            echo "Word: " . implode(' ', $guessedLetters) . PHP_EOL;
+            echo PHP_EOL;
+            echo "Misses: " . implode(' ',$misses) . PHP_EOL;
+            echo PHP_EOL;
+            echo "Guess: " . implode(' ',$guess) . PHP_EOL;
+            while(true){
+                echo PHP_EOL;
+                $input = strtolower(readline('Input a char : '));
+                if (preg_match('/^[A-Z]$/i',$input)){
+                    if(!in_array($input,$guess)&&!in_array($input,$misses)){
+                        break;
+                    } else echo "Char already has been used. Try another!\n";
+                } else echo "Incorrect input. Input 1 char!\n";
             }
-        } else {
-            $misses[] = $input;
-            $guesses++;
-            echo "incorrect letter. guesses tried $guesses/$maxGuesses" . PHP_EOL;
+
+            if (in_array($input, $wordLetters)) {
+                $guess[] = $input;
+                foreach ($wordLetters as $key => $element) {
+                    if ($element === $input) {
+                        $guessedLetters[$key] = $input;
+                    }
+                }
+            } else {
+                $misses[] = $input;
+                $guesses++;
+                echo "incorrect letter. guesses tried $guesses/$maxGuesses" . PHP_EOL;
+            }
+            echo PHP_EOL;
         }
         echo PHP_EOL;
+
+        if ($guesses === $maxGuesses) {
+            echo 'you lose' . PHP_EOL;
+        } else echo 'you win' . PHP_EOL;
+        $pattern = "/^y$|^n$/i";
+        while (true) {
+            $input = readline('Play another game? (Y/N)');
+            if(!preg_match($pattern, $input)){
+                echo "Incorrect choice! type y|Y for yes or n|N for no\n";
+            } else break;
+        }
+        $pattern = "/y/i";
+        if(!preg_match($pattern, $input)){
+            break;
+        };
     }
-    echo PHP_EOL;
-
-    if ($guesses === $maxGuesses) {
-        echo 'you lose' . PHP_EOL;
-    } else echo 'you win' . PHP_EOL;
-
